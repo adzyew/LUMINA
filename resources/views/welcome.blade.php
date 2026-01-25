ebe04345193484f7508fe5cfd70537e9bc1cab5c
Meriel Ady <merieladytalub@gmail.com>
2026-01-26 01:22:27 +0800

    controllers, migrations, folders

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     @vite('resources/css/app.css')
   </head>
-  <body>
-    <h1 class="text-3xl font-bold underline bg-amber-300">
-      Hello world! TANGINA GUMANA NA HAYOP KA
-    </h1>
-    <div class="flex flex-col items-center justify-evenly space-y-6 m-2">
-        <div class="h-50 w-50 rounded-full bg-red-700"></div>
-        <div class="h-50 w-50 rounded-full bg-red-700"></div>
-        <div class="h-50 w-50 rounded-full bg-red-700"></div>
+  <body class ="bg-amber-200">
+    <nav class="p-5 bg-white shadow md:flex md:items-center md:justify-between">
+      <div>
+        <span class="text-2xl font-[Poppins] cursor-pointer" >
+          <img class="h-10 inline" src="https://w7.pngwing.com/pngs/293/485/png-transparent-tailwind-css-hd-logo.png">
+          BURAT
+        </span>
+      </div>
 
-    </div>
+      <ul class="md:flex md:items-center">
+        <li class="mx-4">
+          <a href="#" class="text-xl hover:text-amber-300 duration-500">HOME</a>
+        </li>
+        <li class="mx-4">
+          <a href="#" class="text-xl hover:text-amber-300 duration-500">PRODUCTS</a>
+        </li>
+        <li class="mx-4">
+          <a href="#" class="text-xl hover:text-amber-300 duration-500">ABOUT US</a>
+        </li>
+        <li class="mx-4">
+          <a href="#" class="text-xl hover:text-amber-300 duration-500">CONTACT US</a>
+        </li>
+      </ul>
+    </nav>
   </body>
 </html>
