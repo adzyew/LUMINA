29bb8644c8b4b4cce38ef7a96ac78ce3afab6147
Meriel Ady <merieladytalub@gmail.com>
2026-01-25 17:15:04 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold underline bg-amber-300">
+      Hello world! TANGINA GUMANA NA HAYOP KA
+    </h1>
+    <div class="flex flex-col items-center justify-evenly space-y-6 m-2">
+        <div class="h-50 w-50 rounded-full bg-red-700"></div>
+        <div class="h-50 w-50 rounded-full bg-red-700"></div>
+        <div class="h-50 w-50 rounded-full bg-red-700"></div>
+
+    </div>
+  </body>
+</html>
